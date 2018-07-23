2c6b1b68da4d4ca143cc1e15ae1578f5ec78604a
alvaro cortes <frijoles4me@gmail.com>
2018-07-23 01:02:52 -0700

    edit, add, delete post

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -33,6 +36,28 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Method override middleware
+app.use(methodOverride("_method"));
+
+// Express session midleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 //Index Route
 app.get("/", (req, res) => {
   const title = "Welcome";
@@ -46,9 +71,31 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+//Ideas index Page
+app.get("/ideas", (req, res) => {
+  Idea.find({})
+    .sort({ date: "desc" })
+    .then(ideas => {
+      res.render("ideas/index", {
+        ideas: ideas
+      });
+    });
+});
+
 // Add Idea Form
 app.get("/ideas/add", (req, res) => {
   res.render("ideas/add");
+});
+
+// Edit Idea Form
+app.get("/ideas/edit/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render("ideas/edit", {
+      idea: idea
+    });
+  });
 });
 
 // Proccess Ideas Form
@@ -68,8 +115,39 @@ app.post("/ideas", (req, res) => {
       details: req.body.details
     });
   } else {
-    res.send("passed");
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser).save().then(idea => {
+      req.flash("success_msg", "Idea added");
+      res.redirect("/ideas");
+    });
   }
+});
+
+// Edit Form porcess
+app.put("/ideas/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    //new values
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save().then(idea => {
+      req.flash("success_msg", "Idea Updated");
+      res.redirect("/ideas");
+    });
+  });
+});
+
+// Delete Idea
+app.delete("/ideas/:id", (req, res) => {
+  Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash("success_msg", "Idea removed");
+    res.redirect("/ideas");
+  });
 });
 
 const port = 5000;
